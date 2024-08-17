77801ad4ba5dcb46dbcfa5387a7d8c20c094b61f
Ari Budimn <rhyman18@gmail.com>
2024-08-17 18:00:40 +0700

    feat(users): add address, phone, and driver license entitiy for users

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,9 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'address' => ['nullable', 'string', 'max:255'],
+            'phone_number' => ['nullable', 'string', 'max:20'],
+            'driver_license' => ['nullable', 'string', 'max:20'],
         ];
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'address',
+        'phone_number',
+        'driver_license'
     ];
 
     /**
